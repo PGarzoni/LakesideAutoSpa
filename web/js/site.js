ab8ec7dcd690f342154315350c35ad462a02ed8c
Peter Garzoni <peter.garzoni@gmail.com>
2018-07-14 21:32:02 -0400

    Made navbar fixed but static when the hamburger icon is present and allowed the collapse to work properly if the hamburger icon is visible as well.

--- web/js/site.js
+++ web/js/site.js
@@ -1,46 +1,50 @@
+$(window).resize(function () {
+    if ($(window).width() >= 768) {
+        $('nav').removeClass('navbar-static-top').addClass("navbar-fixed-top");
+        $('body').css("padding-top", 50);
+    } else {
+        $('nav').removeClass('navbar-fixed-top').addClass("navbar-static-top");
+        $('body').css({ "padding-top": 0 });
+    }
+});
+
+
 // Home Nav Button
 $(document).on("click", "#homeBtn", function () {
-    $('#navbar li').each(function () {
-        $(this).removeClass('active');
-    });
-    $(this).parent().addClass('active');
-
     ContentLoader('home');
+    $(this).parent().addClass('active');
 });
 
 // Specials Nav Button
 $(document).on("click", "#specialsBTn", function () {
-    $('#navbar li').each(function () {
-        $(this).removeClass('active');
-    });
-    $(this).parent().addClass('active');
-
     ContentLoader('specials');
+    $(this).parent().addClass('active');
 });
 
 // History Nav Button
 $(document).on("click", "#historyBtn", function () {
-    $('#navbar li').each(function () {
-        $(this).removeClass('active');
-    });
-    $(this).parent().addClass('active');
-
     ContentLoader('history');
+    $(this).parent().addClass('active');
 });
 
 // Fun Facts Nav Button
 $(document).on("click", "#funFactsBtn", function () {
-    $('#navbar li').each(function () {
-        $(this).removeClass('active');
-    });
-    $(this).parent().addClass('active');
-
     ContentLoader('funfacts');
+    $(this).parent().addClass('active');
 });
 
 // Loads html content based on value given
 function ContentLoader(val) {
+    $('#navbar li').each(function () {
+        $(this).removeClass('active');
+    });
+
     $('#content').empty();
+
+    // Collapse nav menu when it is displayed
+    if ($('#hamburger').css('display') == 'block') {
+        $('#navbar').collapse('toggle');
+    }
 
     if (val == 'home') {
         $('#content').load('content/home.html');
