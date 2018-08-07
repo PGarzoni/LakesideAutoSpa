72db74fc3d6e9688447472d7a8996fb39e433ba9
Peter Garzoni <peter.garzoni@gmail.com>
2018-08-07 17:54:05 -0400

    Overhauled contenloader functions to use a closure. Added functionality on page refreshes that the current tab to remain and scoll to the top. Removed header js logic, going with the fixed header.

--- web/js/site.js
+++ web/js/site.js
@@ -1,63 +1,76 @@
-$(document).ready(function () {
-    AdjustHeader();
-    $('#content').load('content/home.html');
-    $(window).resize(function () {
-        AdjustHeader();
-    });
+var cm = ContentManager();
+if (sessionStorage.getItem('tab') == null) {
+    sessionStorage.setItem('tab', 'Home')
+}
+
+$(window).on('load', function () {
+    cm.contentLoader(sessionStorage.getItem('tab'));
 });
 
-function AdjustHeader() {
-    if ($('#hamburger').css('display') == 'block') {
-        $('nav').removeClass('navbar-fixed-top').addClass("navbar-static-top");
-        $('body').css({ "padding-top": 0 });
-    }
-    else {
-        $('nav').removeClass('navbar-static-top').addClass("navbar-fixed-top");
-        $('body').css("padding-top", 50);
-    }
-}
+$(window).on('beforeunload', function () {
+    sessionStorage.setItem('tab', cm.getActiveTab());
+    $(window).scrollTop(0);
+});
 
 // Home Nav Button
 $(document).on("click", "#homeBtn", function () {
-    ContentLoader(this, 'home');
+    cm.contentLoader($(this).text())
 });
 
 // Specials Nav Button
 $(document).on("click", "#specialsBtn", function () {
-    ContentLoader(this, 'specials');
+    cm.contentLoader($(this).text())
 });
 
 // History Nav Button
 $(document).on("click", "#historyBtn", function () {
-    ContentLoader(this, 'history');
+    cm.contentLoader($(this).text())
 });
 
 // Fun Facts Nav Button
 $(document).on("click", "#funFactsBtn", function () {
-    ContentLoader(this, 'funfacts');
+    cm.contentLoader($(this).text())
 });
 
-// Loads html content based on value given
-function ContentLoader(obj, val) {
-    $('#navbar li.active').removeClass(); //Remove the active status from the previously tab
+function ContentManager() {
+    return {
+        loadContent: function (destination) {
+            $('#content').empty().load('content/' + this.pageCheck(destination) + '.html'); // Clear content and load in desired content
+        },
+        setActiveTab: function (target) {
+            $('#navbar li.active').removeClass(); // Clear active tab
 
-    $(obj).parent().addClass('active'); // Set clicked tab to active
+            // Set new tab to active
+            var d = this.pageCheck(target);
+            $('#navbar li a').each(function () {
+                if ($(this).text().toLowerCase().replace(/\s/g, '') == d) {
+                    $(this).parent().addClass('active');
+                }
+            });
+        },
 
-    if ($('#hamburger').css('display') == 'block') {
-        $('#navbar').collapse('toggle'); // Whenever hamburger icon is present, click events will collapse the menu
-    }
+        contentLoader: function (destination) {
+            if ($('#navbar[aria-expanded]').attr('aria-expanded') && $('#hamburger').css('display') == 'block') {
+                $('#navbar').collapse('toggle')
+            }
+            this.loadContent(destination);
+            this.setActiveTab(destination);
+        },
 
-    $('#content').empty(); // Clear content to load desired data
-    if (val == 'home') {
-        $('#content').load('content/home.html');
-    }
-    else if (val == 'specials') {
-        $('#content').load('content/specials.html');
-    }
-    else if (val == 'history') {
-        $('#content').load('content/history.html');
-    }
-    else if (val == 'funfacts') {
-        $('#content').load('content/funfacts.html');
+        getActiveTab: function () {
+            return $('#navbar li.active a').text();
+        },
+        pageCheck: function (page) {
+            var p = page.toLowerCase().replace(/\s/g, ''); // Lowercase and remove spaces
+            switch (p) {
+                case 'home':
+                case 'specials':
+                case 'history':
+                case 'funfacts':
+                    return p;
+                default:
+                    return 'home';
+            }
+        }
     }
 };
